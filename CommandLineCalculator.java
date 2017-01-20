4df06640b7dc43b0ba92c5539b81123727a6f247
Phyllis Sitati <phyllis.sitati@ashesi.edu.gh>
2017-01-20 18:06:27 +0100

    Committing the add functionality

--- CommandLineCalculator.java
+++ CommandLineCalculator.java
@@ -1,5 +1,19 @@
 public class CommandLineCalculator{
   public static void main(String[]args){
+	  int result =0;
+  //Implementing add functionality
+      if(args[0].equalsIgnoreCase("add")){
+      for(int i=1; i<args.length; i++){
+        result =result+Integer.parseInt(args[i]);}
+      System.out.println(result);
+      }
+      else if(args[0].equalsIgnoreCase("sub")){
+        result = Integer.parseInt(args[1]);
+        for(int i=2; i<args.length; i++){
+        result =result- Integer.parseInt(args[i]);
+         }
+        System.out.println(result);
+      }
     
   }
 }
